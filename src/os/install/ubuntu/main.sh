98c0980151b9ca5ba3c81c4f0e5dc4ec977bf55d
Vladimir Dzyuba <dzyuba.v@gmail.com>
2017-12-23 17:18:47 +0100

    Remove redundant software installations for Ubuntu

--- src/os/install/ubuntu/main.sh
+++ src/os/install/ubuntu/main.sh
@@ -10,15 +10,12 @@ update
 upgrade
 
 ./build-essentials.sh
-./../nvm.sh
 
 ./browsers.sh
-./compression_tools.sh
 ./git.sh
 ./image_tools.sh
 ./misc.sh
 ./misc_tools.sh
-./../npm.sh
 ./tmux.sh
 
 ./cleanup.sh
