991e148cc8bf5e333d7def1172791c63760d6c74
Vladimir Dzyuba <dzyuba.v@gmail.com>
2017-12-23 17:14:35 +0100

    Remove redundant software installations for Mac OS

--- src/os/install/macos/image_tools.sh
+++ src/os/install/macos/image_tools.sh
@@ -8,8 +8,6 @@ cd "$(dirname "${BASH_SOURCE[0]}")" \
 
 print_in_purple "\n   Image Tools\n\n"
 
-brew_install "GIMP" "lisanet-gimp" "caskroom/cask" "cask"
-brew_install "ImageAlpha" "imagealpha" "caskroom/cask" "cask"
 brew_install "ImageMagick" "imagemagick --with-webp"
 brew_install "ImageOptim" "imageoptim" "caskroom/cask" "cask"
 brew_install "LICEcap" "licecap" "caskroom/cask" "cask"
--- src/os/install/macos/misc_tools.sh
+++ src/os/install/macos/misc_tools.sh
@@ -9,4 +9,3 @@ cd "$(dirname "${BASH_SOURCE[0]}")" \
 print_in_purple "\n   Miscellaneous Tools\n\n"
 
 brew_install "ShellCheck" "shellcheck"
-brew_install "Visual Studio Code" "visual-studio-code" "caskroom/cask" "cask"
