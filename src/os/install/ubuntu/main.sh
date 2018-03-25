4d5254b673aed7346c2dcc46fdbda1b95b99f28d
Vladimir Dzyuba <dzyuba.v@gmail.com>
2018-03-25 17:11:27 +0200

    Ubuntu: move all package installations to a single script

--- src/os/install/ubuntu/main.sh
+++ src/os/install/ubuntu/main.sh
@@ -9,13 +9,32 @@ cd "$(dirname "${BASH_SOURCE[0]}")" \
 update
 upgrade
 
-./build-essentials.sh
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-# ./browsers.sh
-./git.sh
-./image_tools.sh
-# ./misc.sh
-./misc_tools.sh
-# ./tmux.sh
+print_in_purple "\n   Development essentials \n\n"
 
-./cleanup.sh
+install_package "Build Essential"    "build-essential" 
+install_package "GnuPG archive keys" "debian-archive-keyring"
+install_package "Git"                "git"
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+print_in_purple "\n   Multimedia \n\n"
+
+install_package "ImageMagick" "imagemagick"
+install_package "EXIF tool"   "libimage-exiftool-perl"
+install_package "VLC"         "vlc"
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+print_in_purple "\n   Miscellaneous tools \n\n"
+
+install_package "cURL"       "curl"
+install_package "xclip"      "xclip"
+install_package "ShellCheck" "shellcheck"
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+print_in_purple "\n   Cleanup\n\n"
+
+autoremove
