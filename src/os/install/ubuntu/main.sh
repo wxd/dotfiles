1151b2f1eb1319469031c5aa6a8c96c0b5fc8aaa
Vladimir Dzyuba <v.dzyuba@faktion.com>
2024-02-01 18:06:21 +0100

    Put all Ubuntu installation steps into single file

--- src/os/install/ubuntu/main.sh
+++ src/os/install/ubuntu/main.sh
@@ -2,12 +2,40 @@
 
 cd "$(dirname "${BASH_SOURCE[0]}")" \
     && . "../../utils.sh" \
-    && . "utils.sh"
+    || exit 1
+
+# ----------------------------------------------------------------------
+# | Helper Functions                                                   |
+# ----------------------------------------------------------------------
+
+install_package() {
+
+    declare -r PACKAGE="$2"
+    declare -r PACKAGE_READABLE_NAME="$1"
+
+    if ! package_is_installed "$PACKAGE"; then
+        execute "sudo apt-get install --allow-unauthenticated -qqy $PACKAGE" "$PACKAGE_READABLE_NAME"
+        #                                      suppress output ─┘│
+        #            assume "yes" as the answer to all prompts ──┘
+    else
+        print_success "$PACKAGE_READABLE_NAME"
+    fi
+
+}
+
+package_is_installed() {
+    dpkg -s "$1" &> /dev/null
+}
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-update
-upgrade
+# Resynchronize the package index files from their sources.
+execute "sudo apt-get update -qqy" "APT (update)"
+
+# Install the newest versions of all packages installed.
+execute "export DEBIAN_FRONTEND=\"noninteractive\" \
+         && sudo apt-get -o Dpkg::Options::=\"--force-confnew\" upgrade -qqy" \
+    "APT (upgrade)"
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
@@ -37,4 +65,6 @@ install_package "ShellCheck" "shellcheck"
 
 print_in_purple "\n   Cleanup\n\n"
 
-autoremove
+# Remove packages that were automatically installed to satisfy
+# dependencies for other packages and are no longer needed.
+execute "sudo apt-get autoremove -qqy" "APT (autoremove)"
