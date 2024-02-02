4a06baffaa0a9b89f9156329cdedba729f0a221d
Vladimir Dzyuba <v.dzyuba@faktion.com>
2024-02-02 13:55:17 +0100

    Ensure Touch ID is used when `sudo` is required

--- src/os/preferences/macos.sh
+++ src/os/preferences/macos.sh
@@ -350,6 +350,12 @@ function configure_terminal() {
 
   execute "./set_terminal_theme.applescript" \
       "Set custom terminal theme"
+
+  # Ensure the Touch ID is used when `sudo` is required.
+  if ! grep -q "pam_tid.so" "/etc/pam.d/sudo"; then
+      execute "sudo sh -c 'echo \"auth sufficient pam_tid.so\" >> /etc/pam.d/sudo'" \
+          "Use Touch ID to authenticate sudo"
+  fi
 }
 
 
